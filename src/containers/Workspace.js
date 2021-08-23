02880120827738d30b8ead304fc0b1585ef972bb
Ruben Robles <rubsrc@gmail.com>
2021-08-22 21:43:30 -0600

    fetch auth data second try

--- src/containers/Workspace.js
+++ src/containers/Workspace.js
@@ -1,40 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
 import BasicTable from "../components/BasicTable";
-class Workspace extends React.Component {
 
-  state = {
-    bonds: [],
+const Workspace = (props) => {
+  useEffect(() => {
+    fetchItems();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const [bonds, setBonds] = useState([]);
+
+  const fetchItems = () => {
+    axios.defaults.headers = {
+      "Content-Type": "application/json",
+      Authorization: `Token ${props.token}`,
+    };
+    axios.get("http://127.0.0.1:8000/api/bonds/").then((res) => {
+      const results = res.data.map((row) => ({
+        key: row.id, // add this for the data-row-key
+        ...row,
+      }));
+      setBonds(results);
+    });
   };
+  return (
+    <div>
+      <h1>Buy Bonds</h1>
+      <BasicTable data={bonds} />
+    </div>
+  );
+};
 
-  componentWillReceiveProps(newProps) {
-    if (newProps.token) {
-      axios.defaults.headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Token " + newProps.token,
-      };
-      axios.get("http://127.0.0.1:8000/api/bonds/").then((res) => {
-        const results = res.data.map((row) => ({
-          key: row.id, // add this for the data-row-key
-          ...row,
-        }));
-        this.setState({
-          bonds: results,
-        });
-      });
-    }
-  }
-
-  render() {
-    return (
-      <>
-        <h1>Buy Bonds</h1>
-        <BasicTable data={this.state.bonds} />
-      </>
-    );
-  }
-}
 const mapStateToProps = (state) => {
   return {
     token: state.auth.token,
