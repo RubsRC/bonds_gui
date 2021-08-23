b97cb608be9d39859408ad4c951cc449d66033af
Ruben Robles <rubsrc@gmail.com>
2021-08-22 20:26:32 -0600

    fetch auth data first try

--- src/containers/Workspace.js
+++ src/containers/Workspace.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import axios from 'axios';
-import BasicTable from '../components/BasicTable';
+import React from "react";
+import { connect } from "react-redux";
+import axios from "axios";
+import BasicTable from "../components/BasicTable";
 class Workspace extends React.Component {
 
   state = {
-    bonds: []
-  }
-  componentDidMount() {
-    axios.get('http://127.0.0.1:8000/api/bond/list-create/')
-    .then(res => {
-      const results = res.data.map(row => ({
-         key: row.id, // add this for the data-row-key
-         ...row
-      }))
-      this.setState({
-        bonds: results
+    bonds: [],
+  };
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.token) {
+      axios.defaults.headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Token " + newProps.token,
+      };
+      axios.get("http://127.0.0.1:8000/api/bonds/").then((res) => {
+        const results = res.data.map((row) => ({
+          key: row.id, // add this for the data-row-key
+          ...row,
+        }));
+        this.setState({
+          bonds: results,
+        });
       });
-    })
+    }
   }
 
   render() {
@@ -28,5 +35,10 @@ class Workspace extends React.Component {
     );
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    token: state.auth.token,
+  };
+};
 
-export default Workspace;
+export default connect(mapStateToProps)(Workspace);
